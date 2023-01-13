49b491c59e12cf21931120b09862863b8abc29ce
yusuf <yusuf_ariturk@icloud.com>
2023-01-13 18:06:10 +0300

    Initial Commit

---
+++ yeniii/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yeniii
+//
+//  Created by yusuf ariturk on 1/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
