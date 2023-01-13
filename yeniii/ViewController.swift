9107afc4dc02b295cfb8005c8f3bbd4ac4072d58
yusuf <yusuf_ariturk@icloud.com>
2023-01-13 18:09:43 +0300

    sanan

--- yeniii/ViewController.swift
+++ yeniii/ViewController.swift
@@ -11,12 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        print ("merhaba dünya")
-        print("akhlaskdjal")
+      
     }
 
 //mdcdslmcsş
 }
 
+//lfkdsşkşs
